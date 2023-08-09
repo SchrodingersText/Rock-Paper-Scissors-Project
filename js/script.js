1caa0da31260712d7b81caf500dc2a6b13ab170a
SchrodingersText <130055324+SchrodingersText@users.noreply.github.com>
2023-08-09 15:37:34 +0200

    Update script.js
    Adds working playRound function

--- js/script.js
+++ js/script.js
@@ -2,10 +2,45 @@ function getComputerChoice() {   // function returns either Rock, Paper, or Scis
   const randomNumber = Math.floor(Math.random() * 100)    // the function first generates a random number between 0 and 99
 
   if (randomNumber >= 0 && randomNumber <= 32) {  
-    return "Rock!"    // if the random number is between 0-32, Rock is returned
+    return "Rock"    // if the random number is between 0-32, Rock is returned
   } else if (randomNumber >= 33 && randomNumber <= 66) {
-    return "Paper!"   // if the random number is between 33-66, Paper is returned
+    return "Paper"   // if the random number is between 33-66, Paper is returned
   } else {
-    return "Scissors!"   // if the random number is between 67-99, Scissors is returned
+    return "Scissors"   // if the random number is between 67-99, Scissors is returned
   }
+}
+
+function playRound(playerChoice, computerChoice) {
+    playerChoice = prompt("Enter your choice. Rock, paper, or scissors?").toLowerCase()
+    computerChoice = getComputerChoice()
+    
+    switch (playerChoice) {
+        case "rock":
+            if (computerChoice === "Paper") {
+                return `The computer wins! ${computerChoice} beats ${playerChoice}.`
+            } else if (computerChoice === "Scissors") {
+                return `You win! ${playerChoice} beats ${computerChoice}.`
+            } else {
+                return `It's a tie! ${computerChoice} against ${playerChoice}`
+            }
+            break;
+        
+        case "paper":
+            if (computerChoice === "Scissors") {
+                return `The computer wins! ${computerChoice} beat ${playerChoice}.`
+            } else if (computerChoice === "Rock") {
+                return `You win! ${playerChoice} beats ${computerChoice}.`
+            } else {
+                return `It's a tie! ${computerChoice} against ${playerChoice}`
+            }
+        
+        case "scissors":
+            if (computerChoice === "Rock") {
+                return `The computer wins! ${computerChoice} beats ${playerChoice}.`
+            } else if (computerChoice === "Paper") {
+                return `You win! ${playerChoice} beat ${computerChoice}.`
+            } else {
+                return `It's a tie! ${computerChoice} against ${playerChoice}`
+            }
+    }
 }
